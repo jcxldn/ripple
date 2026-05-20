76b800fff0f1c984d1b15aaa17f379a4f1689ac8
James Cahill <jcahill@ieee.org>
2026-05-20 17:07:40 +0100

    chore/quictls: remove possibility of linking against non quictls openssl

--- lib/msquic.cmake
+++ lib/msquic.cmake
@@ -1,6 +1,10 @@
 if(RIPPLE_USE_EXTERNAL_MSQUIC)
   find_package(msquic REQUIRED)
+  find_package(OpenSSL REQUIRED)
+
 else()
+
+  set(QUIC_TLS_LIB "quictls" CACHE STRING "use quictls (openssl)" FORCE)
 
   FetchContent_Declare(
     msquic
@@ -17,3 +21,9 @@ add_library(ripple::lib::msquic ALIAS ripple_lib_msquic)
 
 target_include_directories(ripple_lib_msquic INTERFACE ${msquic_SOURCE_DIR}/src/inc)
 target_link_libraries(ripple_lib_msquic INTERFACE msquic)
+
+
+add_library(ripple_lib_quictls INTERFACE)
+add_library(ripple::lib::quictls ALIAS ripple_lib_quictls)
+
+target_link_libraries(ripple_lib_quictls INTERFACE OpenSSL)
